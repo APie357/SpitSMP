ecd4c827faaf71ae99cf17067c099f6b68d0dde8
Andrew <andrew@andrewd1.dev>
2024-04-22 21:09:29 -0700

    Clean up code

--- src/main/java/dev/andrewd1/spitsmp/item/SalivaGlandItem.java
+++ src/main/java/dev/andrewd1/spitsmp/item/SalivaGlandItem.java
@@ -1,8 +1,19 @@
 package dev.andrewd1.spitsmp.item;
 
+import dev.andrewd1.spitsmp.util.ModTags;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketItem;
+import dev.emi.trinkets.api.TrinketsApi;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Pair;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public abstract class SalivaGlandItem extends TrinketItem {
     public SalivaGlandItem(Settings settings) {
@@ -11,4 +22,20 @@ public abstract class SalivaGlandItem extends TrinketItem {
 
     public void useMainAbility(MinecraftServer server, ServerPlayerEntity player) { }
     public void useAltAbility(MinecraftServer server, ServerPlayerEntity player) { }
+
+    @Nullable
+    public static SalivaGlandItem getItemFromSlot(MinecraftServer server, ServerPlayerEntity player) {
+        Optional<TrinketComponent> componentOptional = TrinketsApi.getTrinketComponent(player);
+        if (componentOptional.isEmpty()) { return null; }
+
+        TrinketComponent component = componentOptional.get();
+        if (!component.isEquipped(ModTags.Items.salivaGlandPredicate)) { return null; }
+
+        List<Pair<SlotReference, ItemStack>> equipped = component.getEquipped(ModTags.Items.salivaGlandPredicate);
+        if (equipped.isEmpty()) { return null; }
+
+        AtomicReference<SalivaGlandItem> item = new AtomicReference<>();
+        equipped.forEach(slotReferenceItemStackPair -> item.set((SalivaGlandItem) slotReferenceItemStackPair.getRight().getItem()));
+        return item.get();
+    }
 }
--- src/main/java/dev/andrewd1/spitsmp/network/packet/SpitAltAbilityC2SPacket.java
+++ src/main/java/dev/andrewd1/spitsmp/network/packet/SpitAltAbilityC2SPacket.java
@@ -1,32 +1,16 @@
 package dev.andrewd1.spitsmp.network.packet;
 
 import dev.andrewd1.spitsmp.item.SalivaGlandItem;
-import dev.andrewd1.spitsmp.util.ModTags;
-import dev.emi.trinkets.api.SlotReference;
-import dev.emi.trinkets.api.TrinketComponent;
-import dev.emi.trinkets.api.TrinketsApi;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.Pair;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class SpitAltAbilityC2SPacket {
     public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
-        Optional<TrinketComponent> componentOptional = TrinketsApi.getTrinketComponent(player);
-        if (componentOptional.isEmpty()) { return; }
-        TrinketComponent component = componentOptional.get();
-        if (!component.isEquipped(ModTags.Items.salivaGlandPredicate)) { return; }
-        List<Pair<SlotReference, ItemStack>> equipped = component.getEquipped(ModTags.Items.salivaGlandPredicate);
-        if (equipped.isEmpty()) { return; }
-        AtomicReference<SalivaGlandItem> item = new AtomicReference<>();
-        equipped.forEach(slotReferenceItemStackPair -> item.set((SalivaGlandItem) slotReferenceItemStackPair.getRight().getItem()));
-        item.get().useAltAbility(server, player);
+        SalivaGlandItem item = SalivaGlandItem.getItemFromSlot(server, player);
+        if (item == null) { return; }
+        item.useAltAbility(server, player);
     }
 }
